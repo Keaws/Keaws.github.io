b722bbe9bef653bc8dcb04ff91b37aa9003eb88e
Vitalii Kalchuk <vitalii.kalchuk@playtech.com>
2017-05-22 14:49:48 +0300

    VK - add progress and log

--- vk-music-to-youtube/playlist.js
+++ vk-music-to-youtube/playlist.js
@@ -1,7 +1,13 @@
 // Define some variables used to remember state.
-let playlistId, channelId, songs, currentVideoId, currentSongIndex = 0;
+let playlistId, channelId, songs, currentVideoId,
+currentSongIndex = errorsOccured = 0
+errorQueries = [];
 
 const statusEl = document.getElementById('status');
+const progressBarContainer = document.querySelector('.progress');
+const progressBarEl = document.querySelector('.progress-bar-striped');
+const progressText = document.querySelector('.progress-text');
+const playlistButton = document.getElementById('playlist-button');
 
 // After the API loads, call a function to enable the playlist creation form.
 function handleAPILoaded() {
@@ -10,7 +16,11 @@ function handleAPILoaded() {
 
 // Enable the form for creating a playlist.
 function enableForm() {
-  $('#playlist-button').attr('disabled', false);
+  playlistButton.disabled = false;
+}
+
+function disableForm() {
+  playlistButton.disabled = true;
 }
 
 function IsJsonString(str) {
@@ -25,6 +35,9 @@ function IsJsonString(str) {
 }
 
 function importMusic() {
+  disableForm();
+  currentSongIndex = 0;
+
   createPlaylist()
     .then(addSongs);
 }
@@ -38,17 +51,33 @@ function addSongs() {
       console.log(addtoPlayListResponse);
       console.log('added to playlist');
       currentSongIndex++;
+      updateProgressBar();
       if (currentSongIndex < songs.length) {
         addSongs();
       } else {
         console.log('\nfinished');
-        let playlistLink = document.createElement('div');
-        playlistLink.innerHTML = `<a target="_blank" href="https://www.youtube.com/playlist?list=${playlistId}">Open playlist</a>`;
-        statusEl.insertBefore(playlistLink, statusEl.firstChild);
-        currentSongIndex = 0;
+
+        toggleProgressBar();
+        showFailedSongsIfAny();
+        showPlaylistLink();
+
         return;
       }
+    })
+    .catch(err => {
+      console.error(err);
+      errorsOccured++;
+      currentSongIndex++;
+      if (currentSongIndex < songs.length) {
+        addSongs();
+      }
     });
+}
+
+function showPlaylistLink() {
+    let playlistLink = document.createElement('p');
+    playlistLink.innerHTML = `<a target="_blank" href="https://www.youtube.com/playlist?list=${playlistId}">Open playlist</a>`;
+    statusEl.insertBefore(playlistLink, statusEl.firstChild);
 }
 
 // Create a private playlist.
@@ -59,6 +88,8 @@ function createPlaylist() {
     $('#status').html('Invalid data format');
     return;
   }
+
+  showProgressBar();  
 
   var request = gapi.client.youtube.playlists.insert({
     part: 'snippet,status',
@@ -109,8 +140,8 @@ function addToPlaylist() {
       request.execute(function(response) {
         if (response.result) {
             resolve(response.result);
-            let videoAddedEl = document.createElement('div');
-            videoAddedEl.innerText = `Added ${response.result.snippet.title}`;
+            let videoAddedEl = document.createElement('p');
+            videoAddedEl.innerHTML = `<i class="fa fa-music" aria-hidden="true"></i> ${response.result.snippet.title}`;
             statusEl.insertBefore(videoAddedEl, statusEl.firstChild);
         } else {
             reject(response);
@@ -133,13 +164,51 @@ function search(query) {
       request.execute(function(response) {
         var result = response.result;
 
-        if (result) {
+        if (result && result.items && result.items.length > 0) {
+          console.log('SEARCH RESULT:');
+          console.log(result);
           currentVideoId = result.items[0].id.videoId;
           resolve(result);
         } else {
-          $('#status').html('Could not search');
+          let err = document.createElement('p');
+          err.style.color = 'red';
+          err.innerHTML = `<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Search failed for: ${query}`;
+          statusEl.insertBefore(err, statusEl.firstChild);
+          errorQueries.push(query);
           reject(result);
         }
     });
   });
+}
+
+function showProgressBar() {
+  progressBarContainer.style.display = 'block';
+}
+
+function toggleProgressBar() {
+  progressBarEl.classList.toggle('active');
+  progressBarEl.classList.toggle('finished');
+}
+
+function updateProgressBar() {
+  progressText.innerText = `Songs added ${currentSongIndex - errorsOccured}/${songs.length}`;
+}
+
+function showFailedSongsIfAny() {
+    if (errorQueries.length > 0) {
+      let failDetailsEl = document.createElement('details');
+      let failDetailsSummary = document.createElement('summary');
+
+      failDetailsSummary.style.color = 'orangered';
+      failDetailsSummary.innerText = 'Click to see what songs didn\'t make it:';
+      failDetailsEl.appendChild(failDetailsSummary);
+      for (let fail of errorQueries) {
+        let failEl = document.createElement('p');
+        failEl.innerHTML = fail;
+        failEl.style.color = 'orangered';
+        failDetailsEl.appendChild(failEl);
+      }
+
+      statusEl.insertBefore(failDetailsEl, statusEl.firstChild);
+  }
 }
