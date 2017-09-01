db460ec6dbfef862713f746a0dfed697c84fd25a
Vitalii Kalchuk <v.sweak@gmail.com>
2017-09-01 23:03:44 +0300

    git-api Add sorting, extract default filters

--- git-api-client/src/components/Filters/Filters.js
+++ git-api-client/src/components/Filters/Filters.js
@@ -9,6 +9,15 @@ const FILTER_TYPE = {
   LANGUAGE: 'LANGUAGE'
 }
 
+const DEFAULT_FILTERS = {
+  HAS_OPEN_ISSUES: false,
+  HAS_TOPICS: false,
+  STARRED_TIMES: 0,
+  UPDATED_AFTER: null,
+  REPO_TYPE: null,
+  LANGUAGE: null
+}
+
 function Filters ( { filters, languages } ) {
     return (
       <div>
@@ -20,7 +29,9 @@ function Filters ( { filters, languages } ) {
                   <input
                       type="checkbox"
                       checked={filters.HAS_OPEN_ISSUES}
-                      onClick={(e) => this.props.onFilter(FILTER_TYPE.HAS_OPEN_ISSUES, e.target.checked)}/>
+                      onClick={(e) => this.props.onFilter(
+                        FILTER_TYPE.HAS_OPEN_ISSUES, e.target.checked
+                      )}/>
                 </label>
               </li>
               
@@ -30,7 +41,9 @@ function Filters ( { filters, languages } ) {
                   <input
                       type="checkbox"
                       checked={filters.HAS_TOPICS}
-                      onClick={(e) => this.props.onFilter(FILTER_TYPE.HAS_TOPICS, e.target.checked)}/>
+                      onClick={(e) => this.props.onFilter(
+                        FILTER_TYPE.HAS_TOPICS, e.target.checked
+                      )}/>
                 </label>
               </li>
 
@@ -40,7 +53,9 @@ function Filters ( { filters, languages } ) {
                   <input
                       type="number"
                       checked={filters.STARRED_TIMES}
-                      onInput={(e) => this.props.onFilter(FILTER_TYPE.STARRED_TIMES, e.target.value)}/>
+                      onInput={(e) => this.props.onFilter(
+                        FILTER_TYPE.STARRED_TIMES, e.target.value
+                      )}/>
                 </label>
               </li>
 
@@ -50,26 +65,46 @@ function Filters ( { filters, languages } ) {
                   <input
                       type="date"
                       checked={filters.UPDATED_AFTER}
-                      onInput={(e) => this.props.onFilter(FILTER_TYPE.UPDATED_AFTER, e.target.value)}/>
+                      onInput={(e) => this.props.onFilter(
+                        FILTER_TYPE.UPDATED_AFTER, e.target.value
+                      )}/>
                 </label>
               </li>
 
               <li>
                 <span>Type</span>
-                <select onInput={(e) => this.props.onFilter(FILTER_TYPE.REPO_TYPE, e.target.value)}>
-                  <option value="All" selected={!filters.REPO_TYPE || filters.REPO_TYPE === 'All'}>All</option>
-                  <option value="Fork" selected={filters.REPO_TYPE === 'Fork'}>Fork</option>
-                  <option value="Source" selected={filters.REPO_TYPE === 'Source'}>Source</option>
+                <select onInput={(e) => this.props.onFilter(
+                  FILTER_TYPE.REPO_TYPE, e.target.value
+                )}>
+                  <option value="All" 
+                    selected={!filters.REPO_TYPE || filters.REPO_TYPE === 'All'}>
+                    All
+                  </option>
+                  <option value="Fork"
+                    selected={filters.REPO_TYPE === 'Fork'}>
+                    Fork
+                  </option>
+                  <option value="Source" 
+                    selected={filters.REPO_TYPE === 'Source'}>
+                    Source
+                  </option>
                 </select>
               </li>
 
               <li>
                 <span>Language</span>
-                <select onInput={(e) => this.props.onFilter(FILTER_TYPE.LANGUAGE, e.target.value)}>
-                  <option value="All" selected={!filters.LANGUAGE || filters.LANGUAGE === 'All'}>All</option>
+                <select 
+                  onInput={(e) => this.props.onFilter(
+                    FILTER_TYPE.LANGUAGE, e.target.value
+                  )}>
+                  <option value="All"
+                    selected={!filters.LANGUAGE || filters.LANGUAGE === 'All'}>
+                    All
+                  </option>
                   {
                     languages.map(lang => {
-                      return <option value={lang} selected={filters.LANGUAGE === lang}>
+                      return <option value={lang} 
+                        selected={filters.LANGUAGE === lang}>
                         {lang}
                       </option>
                     })
@@ -81,4 +116,4 @@ function Filters ( { filters, languages } ) {
     )
 }
 
-export { FILTER_TYPE, Filters }
+export { FILTER_TYPE, DEFAULT_FILTERS, Filters }
--- git-api-client/src/components/Profile/Profile.js
+++ git-api-client/src/components/Profile/Profile.js
@@ -1,29 +1,22 @@
 import { h, Component } from 'preact';
 import Repo from '../Repo/Repo';
 import API from '../../api/api';
-import { FILTER_TYPE, Filters } from '../Filters/Filters';
+import { FILTER_TYPE, DEFAULT_FILTERS, Filters } from '../Filters/Filters';
 import ReposList from '../ReposList/ReposList';
+import Sorting from '../Sorting/Sorting';
 
 export class Profile extends Component {
 	constructor(props) {
     super(props);
     
-    const defaultFilters = {};
-    defaultFilters[FILTER_TYPE.HAS_OPEN_ISSUES] = false;
-    defaultFilters[FILTER_TYPE.HAS_TOPICS] = false;
-    defaultFilters[FILTER_TYPE.STARRED_TIMES] = 0;
-    defaultFilters[FILTER_TYPE.UPDATED_AFTER] = null;
-    defaultFilters[FILTER_TYPE.REPO_TYPE] = null;
-    defaultFilters[FILTER_TYPE.LANGUAGE] = null;
-
 		this.state = {
 			repos: null,
       loading: true,
-      filters: defaultFilters,
+      filters: DEFAULT_FILTERS,
       languages: [],
       sorting: {
-        by: 'name', //TODO: Extract to soring types module
-        order: 'desc'
+        by: 'name',
+        order: 'asc'
       }
 		}
 	}
@@ -35,7 +28,9 @@ export class Profile extends Component {
 				this.setState({
 					repos,
           loading: false,
-          languages: [...new Set(repos.map(r => r.language).filter(l => l))]
+          languages: [...new Set(
+            repos.map(r => r.language).filter(l => l)
+          )]
         });
         console.log(repos);
 			})
@@ -46,23 +41,62 @@ export class Profile extends Component {
     this.setState({
       filters: { ...this.state.filters, [type]: state }
     });
-
-    console.log(this.state.filters);
   }
 
-  _getFilteredRepos ( { HAS_OPEN_ISSUES, HAS_TOPICS, STARRED_TIMES, UPDATED_AFTER, REPO_TYPE, LANGUAGE}) {
+  _changeSortType(type) {
+    this.setState({
+      sorting: { ...this.state.sorting, by: type }
+    });
+  }
+
+  _changeSortOrder(order) {
+    this.setState({
+      sorting: { ...this.state.sorting, order: order }
+    });
+  }
+
+  _getFilteredRepos( {HAS_OPEN_ISSUES, HAS_TOPICS, STARRED_TIMES, 
+    UPDATED_AFTER, REPO_TYPE, LANGUAGE} )
+  {
     return this.state.repos.filter(repo => {
       return (HAS_OPEN_ISSUES ? !!repo.open_issues_count : true)
         && (HAS_TOPICS ? !!repo.topics.length : true)
         && repo.stargazers_count >= STARRED_TIMES
-        && (UPDATED_AFTER ? new Date(repo.pushed_at) > new Date(UPDATED_AFTER) : true)
-        && (REPO_TYPE && REPO_TYPE !== 'All' ? (REPO_TYPE === 'Fork' ? repo.fork : !repo.fork) : true)
-        && (LANGUAGE && LANGUAGE !== 'All' ? repo.language === LANGUAGE : true)
+        && (UPDATED_AFTER
+           ? new Date(repo.pushed_at) > new Date(UPDATED_AFTER) 
+           : true)
+        && (REPO_TYPE && REPO_TYPE !== 'All' 
+           ? (REPO_TYPE === 'Fork' ? repo.fork : !repo.fork) 
+           : true)
+        && (LANGUAGE && LANGUAGE !== 'All'
+           ? repo.language === LANGUAGE 
+           : true)
     });
   }
 
-	render({}, {loading, repos, filters, languages}) {
+  _sortRepos(repos, {by, order}) {
+    let sorted = [];
+
+    switch (by) {
+      case 'name':
+        sorted = repos.sort((a,b) => a[by].localeCompare(b[by]));
+        break;
+      case 'pushed_at':
+        sorted = repos.sort((a,b) => new Date(a[by]) - new Date(b[by]));
+        break;
+      default:
+        sorted = repos.sort((a,b) => a[by] - b[by]);
+        break;
+    }
+
+    order === 'desc' && sorted.reverse();
+    
+    return sorted;
+  }
+
+	render({}, {loading, repos, filters, languages, sorting}) {
     const filteredRepos = repos && this._getFilteredRepos(filters);
+    const sortedRepos = repos && this._sortRepos(filteredRepos, sorting);
 
 		return (
 			<div>
@@ -71,8 +105,17 @@ export class Profile extends Component {
         			: repos.length > 0
 						? (
 							<div>
-								<Filters languages={languages} filters={filters} onFilter={this._changeFilter.bind(this)} />
-								<ReposList repos={filteredRepos} />
+                <Filters 
+                  languages={languages} 
+                  filters={filters} 
+                  onFilter={this._changeFilter.bind(this)} />
+
+                <Sorting 
+                  sortingParams={sorting} 
+                  onSortType={this._changeSortType.bind(this)} 
+                  onSortOrder={this._changeSortOrder.bind(this)} />
+                  
+                <ReposList repos={sortedRepos} />
 							</div>
 						)
 						: <p>{this.props.user} doesn’t have any public repositories yet.</p>
